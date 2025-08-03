ff7121471240de51a60ef1d782375fc8c59fc559
jasminedirken <r1049280@student.thomasmore.be>
2025-08-03 20:10:22 +0200

    images, html, css, js

---
+++ index.js
@@ -0,0 +1,40 @@
+const images = [
+    'pokemon-widget\assets\animation\animation-1.png',
+    'pokemon-widget\assets\animation\animation-2.png',
+    'pokemon-widget\assets\animation\animation-3-fr.png',
+    'pokemon-widget\assets\animation\animation-4.png'
+];
+
+const imageContent = document.querySelector('.image-content');
+const mainButton = document.getElementById('main-button');
+const finalMessage = document.querySelector('.final-message');
+
+
+let currentIndex = 0;
+function updateImage() {
+    imageContent.style.opacity = 0;
+
+    const img = new Image();
+    img.src = images[currentIndex];
+
+    img.onload = () => {
+        imageContent.style.backgroundImage = `url('${images[currentIndex]}')`;
+
+        imageContent.Stle.opacity = 1;
+    };
+}
+
+updateImage();
+
+mainButton.addEventListener('click', () => {
+    currentIndex++;
+
+    if (currentIndex < images.length) {
+        updateImage();
+    }
+
+    if (currentIndex === images.length - 1) {
+        mainButton.style.display = 'none';
+        finalMessage.style.display = 'block';
+    }
+});
